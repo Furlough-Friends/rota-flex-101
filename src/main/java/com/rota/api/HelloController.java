e2c06a506a05ff8474021db59372b45d6eb58e42
Tadeusz Janowski <tadjanow@gmail.com>
2020-04-06 16:24:53 +0100

    :tada: Initial commit - spring application with a /hello /hello?message= and /hello/message GET endpoints

---
+++ src/main/java/com/rota/api/HelloController.java
@@ -0,0 +1,43 @@
+package com.rota.api;
+
+import com.rota.api.dto.HelloWorld;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+  private AtomicLong id = new AtomicLong();
+  private String template = "Hello %s!";
+
+  /**
+   * /hello will return "Hello World!".
+   * /hello?message=foo witll return "Hello foo!".
+   * @param message Message to print
+   * @return HelloWorld response DTO
+   */
+  @GetMapping("/hello")
+  public HelloWorld helloEndpoint(
+      @RequestParam(value="message",
+          defaultValue="World")
+      String message
+  ) {
+    return new HelloWorld(
+        id.incrementAndGet(),
+        String.format(template, message)
+    );
+  };
+
+  @GetMapping("/hello/{message}")
+  public HelloWorld helloVariableEndpoint(
+      @PathVariable
+      String message
+  ) {
+    return new HelloWorld(
+        id.incrementAndGet(),
+        String.format(template, message)
+    );
+  }
+}
