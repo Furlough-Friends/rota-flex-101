ba0cb63f054b6e2772dbac4f0e2d1498c793e134
Tadeusz Janowski <tadjanow@gmail.com>
2020-04-07 15:20:55 +0100

    :recycle: changed directory structure

---
+++ backend/src/main/java/com/rota/RotaApplication.java
@@ -0,0 +1,13 @@
+package com.rota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotaApplication.class, args);
+	}
+
+}
