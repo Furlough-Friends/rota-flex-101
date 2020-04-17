69811d5234ee2caa85c6c4ece3f39289438caf1f
QuarkNerd <garora1410@yahoo.co.uk>
2020-04-17 10:08:48 +0000

    F49 render home component in app

--- frontend/src/components/app/index.tsx
+++ frontend/src/components/app/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+import Home from '../home';
+const App = () => {
+  return <Home />;
+};
 
 export default App;
