11b011c91749baaffd9134f6aeff14a3a6721dcd
Gurveer Arora <Gurveer.S.Arora@gmail.com>
2020-04-20 15:05:22 +0100

    Feature/49 create home and sidebar (#55)
    *  Remove uneeded files and create structure
    
    * Create sidebar component with buttons
    
    * Create home component with sidebar
    
    * Render home component in app
    
    * Install node-sass

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from './components/app';
 import { store } from './app/store';
 import * as serviceWorker from './serviceWorker';
 
