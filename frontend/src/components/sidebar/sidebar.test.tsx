0558ae67098fd8bfab9a7a0d9a9fd76ee7a50bb8
Tadeusz Janowski <tadjanow@gmail.com>
2020-04-21 15:04:00 +0100

    :white_check_mark: fixed broken unit test

--- frontend/src/components/sidebar/sidebar.test.tsx
+++ frontend/src/components/sidebar/sidebar.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import Sidebar from './index';
 
 test('renders the menu options', () => {
-  const { getByText } = render(<Sidebar selctedOption="Summary" />);
+  const { getByText } = render(<Router><Sidebar selectedOption="Summary"/></Router>);
 
   expect(getByText(/Summary/i)).toBeInTheDocument();
   expect(getByText(/Rota/i)).toBeInTheDocument();
