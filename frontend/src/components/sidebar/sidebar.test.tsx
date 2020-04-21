f43ce20512880db7ee037df8bf51c979c43de65e
Eddy Brown <eddybrown@me.com>
2020-04-21 14:25:35 +0100

    Update sidebar test

--- frontend/src/components/sidebar/sidebar.test.tsx
+++ frontend/src/components/sidebar/sidebar.test.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Sidebar from './index';
+import { useAuth0 } from '../../react-auth0-spa';
+
+jest.mock('../../react-auth0-spa');
+
+const user = {
+  name: 'test@test.com',
+};
+
+beforeEach(() => {
+  (useAuth0 as jest.Mock).mockReturnValue({
+    isAuthenticated: true,
+    user,
+    logout: jest.fn(),
+    loginWithRedirect: jest.fn(),
+  });
+});
 
 test('renders the menu options', () => {
   const { getByText } = render(<Sidebar />);
+  console.log(getByText);
 
   expect(getByText(/Summary/i)).toBeInTheDocument();
   expect(getByText(/Rota/i)).toBeInTheDocument();
   expect(getByText(/Employees/i)).toBeInTheDocument();
+  expect(getByText(/Log out test@test.com/i)).toBeInTheDocument();
 });
