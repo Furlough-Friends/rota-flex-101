4322357cbbebabaaba69bb2accf822c04bcc4eb3
Tadeusz Janowski <tadjanow@gmail.com>
2020-04-21 15:11:43 +0100

    :art: fixed linting

--- frontend/src/components/sidebar/sidebar.test.tsx
+++ frontend/src/components/sidebar/sidebar.test.tsx
@@ -4,7 +4,11 @@ import { render } from '@testing-library/react';
 import Sidebar from './index';
 
 test('renders the menu options', () => {
-  const { getByText } = render(<Router><Sidebar selectedOption="Summary"/></Router>);
+  const { getByText } = render(
+    <Router>
+      <Sidebar selectedOption="Summary"/>
+    </Router>
+  );
 
   expect(getByText(/Summary/i)).toBeInTheDocument();
   expect(getByText(/Rota/i)).toBeInTheDocument();
