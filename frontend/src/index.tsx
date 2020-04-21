96e33bb30136bdc5c8ab9f778c36c02c8e1b164a
Eddy Brown <eddybrown@me.com>
2020-04-21 16:24:08 +0100

    Add appState interface

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,9 +7,15 @@ import config from './auth_config.json';
 import history from './utils/browserHistory';
 import { store } from './app/store';
 import * as serviceWorker from './serviceWorker';
+import { RedirectLoginResult } from '@auth0/auth0-spa-js/dist/typings/global';
+
+interface appState {
+  appState: Promise<RedirectLoginResult>;
+  targetUrl: string;
+}
 
 // Routes user to correct url after authenticating
-const onRedirectCallback = (appState: any) => {
+const onRedirectCallback = (appState: appState) => {  
   history.push(
     appState && appState.targetUrl
       ? appState.targetUrl
