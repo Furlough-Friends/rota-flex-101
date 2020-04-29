f0514e66ba92947f129c30657b549a907a1d348d
Tadeusz Janowski <tadjanow@gmail.com>
2020-04-29 15:56:33 +0100

    :ambulance: fixing some stuff that rebase broke

--- backend/src/main/java/com/rota/api/StaffController.java
+++ backend/src/main/java/com/rota/api/StaffController.java
@@ -104,39 +104,4 @@ public class StaffController {
 
     return ResponseEntity.ok().body(createdStaff);
   }
-
-  /**
-   * Endpoint to create and enter a new staff record into the system.
-   * Staff ID and role is inferred from the token passed in the header's Authorization field.
-   * @param authString Authentication token.
-   * @param createStaffForm Staff details.
-   * @return
-   */
-  @PostMapping("/staff/create")
-  @ApiOperation(value = "Lets an authenticated manager create a new staff user",
-      consumes = "application/json")
-  public ResponseEntity createStaff(
-      @RequestHeader("Authorization")
-      @ApiParam(value = "Authentication token")
-          String authString,
-
-      @Valid
-      @RequestBody
-      @ApiParam("Staff details for the new user")
-          CreateStaffForm createStaffForm
-  ) {
-    final Role role = AuthenticationUtils
-        .getUserRoleFromToken(authString)
-        .orElseThrow(() ->
-          new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Failed to authorize.")
-        );
-
-    if (role != Role.MANAGER) {
-      throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Not enough permissions.");
-    }
-    // Assuming in general staff entered will be active ones
-    Staff createdStaff = staffService.createStaff(createStaffForm.toStaff());
-
-    return ResponseEntity.ok().body(createdStaff);
-  }
 }
