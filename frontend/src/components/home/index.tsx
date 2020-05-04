1edd12d78894832159888cfec64fcf75d3ae2080
Eddy Brown <eddybrown@me.com>
2020-05-04 11:41:16 +0100

    Add PageNotFound to Home

--- frontend/src/components/home/index.tsx
+++ frontend/src/components/home/index.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Sidebar from '../sidebar';
 import Employees from '../employees';
 import Rota from '../rota';
 import Summary from '../summary';
 import homeStyles from './home.module.scss';
+import PageNotFound from '../pageNotFound';
 
 const Home = () => (
   <>
     <div className={homeStyles.home}>
-      <Route path="/employees" component={Employees} />
-      <Route path="/rota" component={Rota} />
-      <Route path="/summary" component={Summary} />
-      <Route path="/" component={Sidebar} />
+      <Sidebar />
+      <Switch>
+        <Route path="/employees" component={Employees} />
+        <Route path="/rota" component={Rota} />
+        <Route path="/summary" component={Summary} />
+        <Route exact path="/">
+          <Redirect to="/employees" />
+        </Route>
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
-    <Route exact path="/">
-      <Redirect to="/employees" />
-    </Route>
   </>
 );
 
