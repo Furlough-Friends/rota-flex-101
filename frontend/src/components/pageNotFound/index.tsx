b494f6cf7fe12fed6a415ca4e48ee8e9e68b0503
Eddy Brown <eddybrown@me.com>
2020-05-05 11:23:32 +0100

    Remove unneeded Grid

--- frontend/src/components/pageNotFound/index.tsx
+++ frontend/src/components/pageNotFound/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Box } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import CancelIcon from '@material-ui/icons/CancelRounded';
 import { useLocation } from 'react-router-dom';
 import errorPageStyle from './pageNotFound.module.scss';
@@ -8,27 +8,21 @@ const PageNotFound = () => {
   const location = useLocation();
   return (
     <div className={errorPageStyle.errorPage}>
-      <Grid
-        container
-        alignItems="center"
-        justify="center"
-        style={{ minHeight: '100vh' }}>
-        <Box>
-          <h1>
-            <CancelIcon
-              className={errorPageStyle.icon}
-              style={{ fontSize: '90px' }}
-            />
-            SORRY
-          </h1>
-          <h2>We could not find that page</h2>
-          <h4>
-            Error: page{' '}
-            <span className={errorPageStyle.path}>{location.pathname}</span> was
-            not found.
-          </h4>
-        </Box>
-      </Grid>
+      <Box>
+        <h1>
+          <CancelIcon
+            className={errorPageStyle.icon}
+            style={{ fontSize: '90px' }}
+          />
+          SORRY
+        </h1>
+        <h2>We could not find that page</h2>
+        <h4>
+          Error: page{' '}
+          <span className={errorPageStyle.path}>{location.pathname}</span> was
+          not found.
+        </h4>
+      </Box>
     </div>
   );
 };
