90b2b368564a861962273f750fd2a7b242be9095
Adrian Borg <borgadrian@yahoo.co.uk>
2020-05-08 00:01:23 +0100

    :lipstick: added icons & sized header

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import headerStyles from './header.module.scss';
 import LoginOutButton from '../loginOutButton';
+
 
 const Header = () => {
   const location = useLocation();
   return (
     <>
       <div className={`${headerStyles.header} ${headerStyles.sidebarHeader}`}>
-        sdfa
+        <ChevronLeftIcon/>
       </div>
+      
       <div className={`${headerStyles.header} ${headerStyles.headerContent}`}>
-        <div className={headerStyles.headerTitle}>
+        <div className={headerStyles.headerLeft}>
+          <MenuIcon/>
+        </div>
+        <div className={headerStyles.headerMiddle}>
           {location.pathname}
         </div>
-        <div className={headerStyles.headerAccount}>
+        <div className={headerStyles.headerRight}>
           <LoginOutButton />
         </div>
       </div>
