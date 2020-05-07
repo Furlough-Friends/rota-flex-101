da5e5eb144e6602344aadc50b35c610146c69e91
QuarkNerd <garora1410@yahoo.co.uk>
2020-05-07 15:40:46 +0100

    add rootModal component to app

--- frontend/src/components/app/index.tsx
+++ frontend/src/components/app/index.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Home from '../home';
+import RootModal from '../rootModal';
 
-const App = () => <Home />;
+const App = () => (
+  <>
+    <Home />
+    <RootModal />
+  </>
+);
 
 export default App;
