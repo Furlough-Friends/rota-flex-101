aca9c9f1b9500fd09cfda0429e0a55069292c41a
Eddy Brown <eddybrown@me.com>
2020-05-11 11:36:54 +0100

    Update app component
    Now shows loader when loading
    Shows landing page if user is not logged in

--- frontend/src/components/app/index.tsx
+++ frontend/src/components/app/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import Home from '../home';
+import Landing from '../landing';
+import { useAuth0 } from '../../react-auth0-spa';
+import { CircularProgress } from '@material-ui/core';
+import style from './index.module.scss';
 
-const App = () => <Home />;
+const App = () => {
+  const { isAuthenticated, loading } = useAuth0();
+  if (loading)
+    return (
+      <div className={style.loaderWrapper}>
+        <CircularProgress className={style.loader} />
+      </div>
+    );
+  else return isAuthenticated ? <Home /> : <Landing />;
+};
 
 export default App;
