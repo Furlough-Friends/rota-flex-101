2814070a840cbc01acd340c1ad2bbdcc6ce5c464
Eddy Brown <eddybrown@me.com>
2020-05-14 22:30:13 +0100

    Remove unused imports

--- backend/src/main/java/com/rota/database/orm/engagement/Engagement.java
+++ backend/src/main/java/com/rota/database/orm/engagement/Engagement.java
@@ -14,8 +14,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 @Data
 @Builder
