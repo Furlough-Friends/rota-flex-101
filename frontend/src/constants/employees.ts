1c275cca632fa7cb05a6cb95b7bf745e28af0a8d
QuarkNerd <garora1410@yahoo.co.uk>
2020-05-14 11:08:14 +0100

    create interface for sending new user data

--- frontend/src/constants/employees.ts
+++ frontend/src/constants/employees.ts
@@ -1,15 +1,26 @@
 import { URL } from './global';
+import { DateStr, toDateStr } from './global'
 
 export const STAFF_FETCH_URL = `${URL}/staff/get`;
 export const STAFF_DELETE_URL = `${URL}/staff/remove?id=`;
 
-export interface StaffData {
-  id: number;
+interface BaseStaffData {
   firstName: string;
   surname: string;
   jobTitle: string;
   contractedHours: number;
 }
+
+export interface StaffData extends BaseStaffData {
+  id: number;
+}
+
+export interface CreateStaffData extends BaseStaffData {
+  startDate: DateStr;
+  role: "USER" | "MANAGER";
+  preferredDates: string;
+}
+
 export interface TableColumn {
   id: string;
   name: string;
--- frontend/src/constants/global.ts
+++ frontend/src/constants/global.ts
@@ -3,3 +3,26 @@ export const FULLTIME_HOURS = 37.5;
 
 // A placeholder for authentication token
 export const getAuthenticationToken = (): string => 'xx';
+
+enum DateStrBrand {}
+
+export type DateStr = string & DateStrBrand;
+
+function checkValidDateStr(str: string): str is DateStr {
+  return str.match(/^\d{4}-\d{2}-\d{2}$/) !== null;
+}
+
+export function toDateStr(date: Date | string): DateStr {
+  if (typeof date === 'string') {
+    if (checkValidDateStr(date)) {
+      return date;
+    }
+    throw new Error(`Invalid date string: ${date}`);
+  } else {
+    const dateString = date.toISOString().split('T')[0];
+    if (checkValidDateStr(dateString)) {
+      return dateString;
+    }
+  }
+  throw new Error(`Shouldn't get here (invalid toDateStr provided): ${date}`);
+}
