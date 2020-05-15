b886f89c183d114500567ba90ab1ec5059846500
Adrian Borg <aborg@scottlogic.com>
2020-05-15 06:31:17 +0100

    Feature/53 make sidebar collapsible (#103)
    * :sparkles: sidebar buttons work, opening and closing the sidebar
    * :recycle: :lipstick: added header & moved login button to header
    * :lipstick: :wheelchair:  modified overall main grid to allow header and renamed/reordered elements for better accessibility
    * :lipstick: Using MaterialUI drawers for a colapsible menu
    Also some styling
    * :iphone: made page layout change when under 780px
    drawer icon and anchor location changed
    * :art: using NavLink to replace buttons and Link for sidebar options

--- frontend/src/components/home/index.tsx
+++ frontend/src/components/home/index.tsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
-import Sidebar from '../sidebar';
+import Header from '../header';
 import Employees from '../employees';
 import Rota from '../rota';
+import Sidebar from '../sidebar';
 import Summary from '../summary';
 import homeStyles from './home.module.scss';
 import PageNotFound from '../pageNotFound';
 
 const Home = () => (
-  <>
-    <div className={homeStyles.home}>
-      <Sidebar />
-      <Switch>
-        <Route path="/employees" component={Employees} />
-        <Route path="/rota" component={Rota} />
-        <Route path="/summary" component={Summary} />
-        <Route exact path="/">
-          <Redirect to="/summary" />
-        </Route>
-        <Route path="/callback" />
-        <Route component={PageNotFound} />
-      </Switch>
+  <div className={homeStyles.home}>
+    <Sidebar />
+    <div className={`${homeStyles.right} ${homeStyles.column}`}>
+      <Header />
+      <main className={homeStyles.main}>
+        <Switch>
+          <Route path="/employees" component={Employees} />
+          <Route path="/rota" component={Rota} />
+          <Route path="/summary" component={Summary} />
+          <Route exact path="/">
+            <Redirect to="/employees" />
+          </Route>
+          <Route path="/callback" />
+          <Route component={PageNotFound} />
+        </Switch>
+      </main>
     </div>
-  </>
+  </div>
 );
 
 export default Home;
--- frontend/src/constants/global.ts
+++ frontend/src/constants/global.ts
@@ -1,5 +1,7 @@
 export const URL = 'http://localhost:8080';
 export const FULLTIME_HOURS = 37.5;
 
+export const WINDOW_WIDTH_THRESHOLD: number = 780;
+
 // A placeholder for authentication token
 export const getAuthenticationToken = (): string => 'xx';
