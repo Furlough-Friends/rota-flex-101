486edf533aaaaec249201de4639749c37c074cb8
Eddy Brown <eddybrown@me.com>
2020-05-19 09:49:52 +0100

    Remove unused imports

--- backend/src/main/java/com/rota/exceptions/ControllerExceptionHandler.java
+++ backend/src/main/java/com/rota/exceptions/ControllerExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.rota.exceptions;
 
-import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
