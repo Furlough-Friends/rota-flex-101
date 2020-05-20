6027131135c8c0ae7bc721c4d1dfd8664670a00a
Eddy Brown <eddybrown@me.com>
2020-05-20 13:01:28 +0100

    Move full page loader to own component

---
+++ frontend/src/components/fullPageLoader/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import style from './fullPageLoader.module.scss';
+
+const FullPageLoader = () => (
+  <div className={style.loaderWrapper}>
+    <CircularProgress className={style.loader} />
+  </div>
+);
+
+export default FullPageLoader;
