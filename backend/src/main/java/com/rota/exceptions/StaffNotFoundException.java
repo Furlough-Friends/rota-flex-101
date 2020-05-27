8573fa79893be9fdcc7620d7abc681f8a4a0ec75
tjanowsk <63243951+tjanowsk@users.noreply.github.com>
2020-05-27 14:16:30 +0100

    Feature/37 - Get engagements between dates endpoint (#131)
    * :tada: staff/shifts endpoint initial commit
    
    * :white_check_mark: added unit test
    
    * :white_check_mark: :ok_hand: added more tests, fixed comments

--- backend/src/main/java/com/rota/exceptions/StaffNotFoundException.java
+++ backend/src/main/java/com/rota/exceptions/StaffNotFoundException.java
@@ -1,6 +1,8 @@
 package com.rota.exceptions;
 
 public class StaffNotFoundException extends RuntimeException {
+  static final long serialVersionUID = 1L;
+
   public StaffNotFoundException(int id) {
     super("Unable to find staff member with ID: " + id);
   }
