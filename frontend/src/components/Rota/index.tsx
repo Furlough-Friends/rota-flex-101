11aacd37b7446e59c738aeca4812373c3112b6b8
Tadeusz Janowski <tadjanow@gmail.com>
2020-06-08 17:10:14 +0100

    :sparkles: the manager can view existing events

--- frontend/src/components/Rota/index.tsx
+++ frontend/src/components/Rota/index.tsx
@@ -6,31 +6,42 @@ import 'tippy.js/dist/tippy.css';
 import { EventApi } from '@fullcalendar/core';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import React from 'react';
+import React, { useEffect } from 'react';
 import tippy from 'tippy.js';
+import toastr from 'toastr';
 
+import { useDispatch, useSelector } from 'react-redux';
 import rotaStyle from './rota.module.scss';
+import {
+  selectEngagement,
+  fetchEngagements,
+} from '../../store/engagementSlice';
+import { useAuth0 } from '../../auth0Spa';
+import { Engagement, Employee } from '../../model';
+import { fetchEmployee, selectEmployees } from '../../store/employeeSlice';
 
-const DUMMY_EVENTS = [
-  {
-    start: '2020-05-08T08:39:14Z',
-    end: '2020-05-08T19:41:14Z',
-    title: 'slacking off plz pay',
-    description: 'no, seriously, I need that money',
-    color: 'seagreen',
-  },
-  {
-    start: '2020-05-08T11:28Z',
-    end: '2020-05-08T13:33Z',
-    title: 'lunch',
-    description: 'FOOOD',
-    color: 'gold',
-  },
-];
+const COLOUR_CYCLES = ['red', 'green', 'blue', 'cyan', 'magenta', 'yellow'];
+
+const engagementToCalendarEvent = (employeeList: Employee[]) => ({
+  start,
+  end,
+  staffId,
+  type,
+}: Engagement) => {
+  const employee = employeeList.find(staff => staff.id === staffId);
+  return {
+    start,
+    end,
+    title: employee?.surname,
+    description: `<p>${employee?.firstName} ${employee?.surname}<p>From: ${start}<p>To: ${end}<p>Type: ${type}`,
+    color: COLOUR_CYCLES[staffId % COLOUR_CYCLES.length],
+  };
+};
 
 const tooltipFunction = (info: { event: EventApi; el: HTMLElement }) => {
   tippy(info.el, {
     content: info.event.extendedProps.description,
+    allowHTML: true,
   });
 };
 
@@ -48,24 +59,48 @@ const BUTTON_TEXT = {
   list: 'List',
 };
 
-const Rota = () => (
-  <div className={rotaStyle.rota}>
-    <h1 className={rotaStyle.header}> Rota </h1>
-    <div className={rotaStyle.calendarContainer}>
-      <FullCalendar
-        plugins={[timeGridPlugin]}
-        height="parent"
-        locale={navigator.language}
-        allDaySlot={false}
-        slotLabelFormat={TIME_FORMAT}
-        minTime="08:00:00"
-        maxTime="21:00:00"
-        events={DUMMY_EVENTS}
-        buttonText={BUTTON_TEXT}
-        eventRender={tooltipFunction}
-      />
+const Rota = () => {
+  const engagementList = useSelector(selectEngagement);
+  const employeeList = useSelector(selectEmployees);
+  const dispatch = useDispatch();
+  const { getTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    const getEmployeesAndEngagements = async () => {
+      const accessToken = await getTokenSilently();
+      dispatch(fetchEmployee(accessToken));
+      dispatch(fetchEngagements(new Date(0), new Date(1e14), accessToken));
+    };
+    getEmployeesAndEngagements();
+  }, [dispatch, getTokenSilently]);
+  return (
+    <div className={rotaStyle.rota}>
+      <h1 className={rotaStyle.header}> Rota </h1>
+      <div className={rotaStyle.calendarContainer}>
+        <FullCalendar
+          plugins={[timeGridPlugin]}
+          height="parent"
+          locale={navigator.language}
+          allDaySlot={false}
+          slotLabelFormat={TIME_FORMAT}
+          minTime="08:00:00"
+          maxTime="21:00:00"
+          events={engagementList.map(engagementToCalendarEvent(employeeList))}
+          buttonText={BUTTON_TEXT}
+          eventRender={tooltipFunction}
+          customButtons={{
+            add: {
+              text: 'Add',
+              click: () => toastr.info('Add engagement clicked'),
+            },
+          }}
+          header={{
+            right: 'add prev,today,next',
+          }}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Rota;
