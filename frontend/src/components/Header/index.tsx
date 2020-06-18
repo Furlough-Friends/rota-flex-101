37bb49ac5c7e1cc9cc6c78bfb2bbbdecdf3dab3e
Eddy Brown <eddybrown@me.com>
2020-06-18 10:14:12 +0100

    Add user email to header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 import LoginOutButton from '../LoginOutButton';
 import headerStyles from './header.module.scss';
+import { useAuth0 } from '../../auth0Spa';
 
-const Header = () => (
-  <header className={headerStyles.headerContent}>
-    <div className={headerStyles.headerRight}>
-      <LoginOutButton />
-    </div>
-  </header>
-);
+const Header = () => {
+  const { user } = useAuth0();
+  return (
+    <header className={headerStyles.headerContent}>
+      <div className={headerStyles.headerRight}>
+        <p className={headerStyles.userName}>{user.nickname}</p>
+        <LoginOutButton />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
